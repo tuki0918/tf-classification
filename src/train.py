2035b0a76acea7f81b30f96767c9d49b3b43beeb
tuki0918 <tuki0918@gmail.com>
2017-03-06 05:41:10 +0900

    add: result predict label
    result: “filename” predict “classname” class.

--- src/train.py
+++ src/train.py
@@ -20,22 +20,28 @@ FLAGS = None
 def train():
         train_generator, validation_generator = generator()
 
-        # モデルを構築
         model = Sequential()
+
+        # conv1
         model.add(Convolution2D(32, 3, 3, border_mode='same', activation='relu', input_shape=(64, 64, 3)))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
+        # conv2
         model.add(Convolution2D(64, 3, 3, border_mode='same', activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
 
+        # one dim
         model.add(Flatten())
 
+        # fc1
         model.add(Dense(64, activation='relu'))
         model.add(Dropout(0.5))
 
-        # nb_class ... length of classifications: training dataset
+        # fc2
+        # dim ... length of classifications: training dataset
         model.add(Dense(train_generator.nb_class, activation='softmax'))
 
+        # model visualize
         model.summary()
 
         # https://keras.io/ja/optimizers/
@@ -44,40 +50,46 @@ def train():
                       optimizer='adam',
                       metrics=['accuracy'])
 
-        # 訓練
+        # learning
         history = model.fit_generator(
                 train_generator,
-                samples_per_epoch=2000,
+                samples_per_epoch=500,
                 nb_epoch=FLAGS.nb_epoch,
                 validation_data=validation_generator,
-                nb_val_samples=800)
+                nb_val_samples=200)
 
-        # 成果物
+        # model store
         model.save('model.h5')
         del model
 
 
 def predict():
-        model = load_model('model.h5')
+        # label list: training dataset
+        train_generator, validation_generator = generator()
+        labels = dict((v, k) for k, v in train_generator.class_indices.items())
 
+        # model load
+        model = load_model('model.h5')
         model.summary()
 
+        # test data: don't shuffle
         datagen = ImageDataGenerator(rescale=1./255)
-        generator = datagen.flow_from_directory(
+        test_generator = datagen.flow_from_directory(
                 'data/test',
+                shuffle=False,
                 target_size=(64, 64),
                 batch_size=32,
                 class_mode='categorical')
 
-        predictions = model.predict_generator(generator, generator.nb_sample)
+        # predict
+        predictions = model.predict_generator(test_generator, test_generator.nb_sample)
 
-        print(predictions)
-        print(np.argmax(predictions, axis=1))
-        print(predictions.round())
+        # best label index
+        y = np.argmax(predictions, axis=1)
 
-        # out = model.predict(im)
-        # best_guess = labels[np.argmax(out)]
-        # print(model.predict(training_data).round())
+        # result
+        for (file, index) in zip (test_generator.filenames, y):
+                print('result: "%s" predict "%s" class.' % (file, labels[index]))
 
 
 if __name__ == '__main__':
